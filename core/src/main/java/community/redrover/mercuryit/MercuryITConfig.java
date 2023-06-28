452952fd307638da24457cab9b23d1207d4c93b2
aleks <goluublue@gmail.com>
2023-06-28 18:39:21 -0400

    created initial JavaDocs for config-related classes

--- core/src/main/java/community/redrover/mercuryit/MercuryITConfig.java
+++ core/src/main/java/community/redrover/mercuryit/MercuryITConfig.java
@@ -9,11 +9,22 @@ import org.apache.commons.configuration2.ex.ConfigurationException;
 
 import java.util.TreeMap;
 
-
+/**
+ * Contains properties relating to a particular aspect of test setup.
+ */
 public abstract class MercuryITConfig extends MercuryITObject<MercuryITConfig> {
 
     private static Configuration configuration;
 
+    /**
+     * Creates a Configuration object if none exists, based on the first matching file in the directory "test/resources", in the following list of priorities:
+     * <ol>
+     *      <li>.yaml file </li>
+     *      <li>.properties file</li>
+     *      <li> New, empty configuration object with no file source</li>
+     * </ol>
+     * @return the existing configuration, or a new configuration if none is found
+     */
     protected static Configuration configuration() {
         if (configuration == null) {
             Configurations configurations = new Configurations();
@@ -33,6 +44,11 @@ public abstract class MercuryITConfig extends MercuryITObject<MercuryITConfig> {
 
     public static final String APP_NAME = "mercuryit";
 
+    /**
+     * Constructs a property name to be retrieved from the configuration's map; intended to be used by the configuration's getString() method.
+     * @param names components that are combined to generate the property name
+     * @return a property name, formatted in the style of a configuration key
+     */
     public static String name(String... names) {
         StringBuilder fullName = new StringBuilder();
         for (String name : names) {
@@ -50,8 +66,19 @@ public abstract class MercuryITConfig extends MercuryITObject<MercuryITConfig> {
         super(configHolder);
     }
 
+    /**
+     * Generates a copy of the config, stores a reference to the given ConfigHolder inside of it, and adds the config into the specified ConfigHolder's map.
+     * @param configHolder the ConfigHolder the copy of this object will belong to
+     * @return the generated copy of this config
+     */
     protected abstract MercuryITConfig copy(MercuryITConfigHolder configHolder);
 
+    /**
+     * Used to generate an API request
+     * @param clazz type of the API request to be generated
+     * @return a generated API request
+     * @param <Request> type of the API request to be generated
+     */
     @Override
     public <Request extends MercuryITRequest<Request>> Request request(Class<Request> clazz) {
         return super.request(clazz);
--- core/src/main/java/community/redrover/mercuryit/MercuryITConfigHolder.java
+++ core/src/main/java/community/redrover/mercuryit/MercuryITConfigHolder.java
@@ -5,7 +5,9 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
-
+/**
+ * Contains a set of testing configurations in an internal map; only one ConfigHolder exists for a given test.
+ */
 @SuppressWarnings("unchecked")
 public class MercuryITConfigHolder {
 
@@ -15,14 +17,30 @@ public class MercuryITConfigHolder {
         this.configMap = new ConcurrentHashMap<>();
     }
 
+    /**
+     * Uses the Map interface's put method to add or update to this ConfigHolder's map
+     * @param clazz class of the provided configuration; used as key for the map
+     * @param config configuration to be added or updated; used as value for the map
+     * @param <Config> The type of the configuration to be added.
+     */
     <Config extends MercuryITConfig> void set(Class<Config> clazz, Config config) {
         configMap.put(clazz, config);
     }
 
+    /**
+     * Retrieves a config of the given class; if a matching config does not already exist, also creates then adds such a config to the map.
+     * @param clazz class of the config to be retrieved or generated
+     * @return a configuration of the requested type
+     * @param <Config> The type of the configuration to be retrieved.
+     */
     public <Config extends MercuryITConfig> Config config(Class<Config> clazz) {
         return (Config) configMap.computeIfAbsent(clazz, configClass -> MercuryITHelper.create(configClass, new Class[]{MercuryITConfigHolder.class}, new Object[]{this}));
     }
 
+    /**
+     * Creates a copy of this ConfigHolder, containing all entries in its map. This is used for giving a new Request all existing configuration data.
+     * @return an identical copy of this ConfigHolder
+     */
     MercuryITConfigHolder copy() {
         MercuryITConfigHolder mercuryITConfigHolder = new MercuryITConfigHolder();
         mercuryITConfigHolder.configMap.putAll(
