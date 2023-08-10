9ee4022df7959a4f2d6f4e850b3a9803cd219ef0
SergeiDemyanenko <Sergei.Demyanenko@gmail.com>
2023-08-09 22:50:01 -0500

    UTest refactoring (#41)
    UTest refactoring

--- mongo/src/test/java/community/redrover/mercuryit/MercuryITMongoConfigTest.java
+++ mongo/src/test/java/community/redrover/mercuryit/MercuryITMongoConfigTest.java
@@ -1,28 +1,46 @@
 package community.redrover.mercuryit;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MercuryITMongoConfigTest {
 
-    public static final String INPUT_URI = "uri1";
-    public static final String INPUT_DATABASE = "database1";
+    public static final String INPUT_URI = "uri-const";
+    public static final String INPUT_DATABASE = "database-const";
 
     @Test
     @Order(1)
-    public void testMongoConfigDefault() {
-        MercuryITMongoConfig globalMongoConfig = MercuryIT.globalConfig(MercuryITMongoConfig.class);
-        Assertions.assertNull(globalMongoConfig.getUri());
-        Assertions.assertNull(globalMongoConfig.getDatabase());
+    public void testMongoGlobalConfigFromFile() {
+        MercuryITMongoConfig mongoConfig = MercuryIT.globalConfig(MercuryITMongoConfig.class);
+
+        Assertions.assertEquals("uri-file", mongoConfig.getUri());
+        Assertions.assertEquals("database-file", mongoConfig.getDatabase());
     }
 
     @Test
     @Order(2)
-    public void testSqlConfigInput(){
+    public void testMongoGlobalConfigInput() {
         MercuryITMongoConfig mongoConfig = MercuryIT.globalConfig(MercuryITMongoConfig.class)
                 .uri(INPUT_URI)
-                .database(INPUT_DATABASE)
+                .database(INPUT_DATABASE);
+
+        Assertions.assertEquals(INPUT_URI, mongoConfig.getUri());
+        Assertions.assertEquals(INPUT_DATABASE, mongoConfig.getDatabase());
+    }
+
+    @Test
+    @Order(3)
+    public void testMongoGlobalConfigGet() {
+        MercuryITMongoConfig mongoConfig = MercuryIT.globalConfig(MercuryITMongoConfig.class);
+
+        Assertions.assertEquals(INPUT_URI, mongoConfig.getUri());
+        Assertions.assertEquals(INPUT_DATABASE, mongoConfig.getDatabase());
+    }
+
+    @Test
+    @Order(4)
+    public void testMongoGlobalConfigToContext() {
+        MercuryITMongoConfig mongoConfig = MercuryIT
                 .request(MercuryITMongo.class)
                 .contextConfig(MercuryITMongoConfig.class);
 
